47c766e4c30f69e2276b26faa94707d66a238d6b
Sidnei Klein <sidneiklein@gmail.com>
2020-05-04 17:14:23 -0300

    Implementação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (typeof value !== 'number') {
+      throw Error('The Value is not a Number!');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Insufficient balance for the transaction!');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Transaction Type are Inválid!');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    //
+    return transaction;
   }
 }
 
